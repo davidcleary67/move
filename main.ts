a596285e7ccca4825429b8b118b1b3cd237a1a33
davidcleary67 <87801023+davidcleary67@users.noreply.github.com>
2022-05-24 18:29:35 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,97 @@
 function lt (degrees: number) {
-    timeToWait = degrees * MICROSECINASEC / DEGREESPERSEC
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    timeToWait = degrees * MICROSECINASEC / DEGREESPERSECLT
     pins.servoWritePin(AnalogPin.P1, 45)
     pins.servoWritePin(AnalogPin.P2, 45)
     control.waitMicros(timeToWait)
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
+    basic.clearScreen()
+}
+// pins.servoWritePin(AnalogPin.P2, 90)
+function stop () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
 }
 input.onButtonPressed(Button.A, function () {
+    pause(1000)
     for (let index = 0; index < 4; index++) {
         fd(100)
+        pause(1000)
         lt(90)
+        pause(1000)
     }
 })
 function rt (degrees: number) {
-    timeToWait2 = degrees * MICROSECINASEC / DEGREESPERSEC
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    timeToWait2 = degrees * MICROSECINASEC / DEGREESPERSECRT
     pins.servoWritePin(AnalogPin.P1, 135)
     pins.servoWritePin(AnalogPin.P2, 135)
     control.waitMicros(timeToWait2)
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
+    basic.clearScreen()
 }
 function bd (distance: number) {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
     timeToWait4 = distance * MICROSECINASEC / DISTANCEPERSEC
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 0)
     control.waitMicros(timeToWait4)
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
+    basic.clearScreen()
 }
 function fd (distance: number) {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
     timeToWait3 = distance * MICROSECINASEC / DISTANCEPERSEC
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
     control.waitMicros(timeToWait3)
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
+    basic.clearScreen()
 }
 input.onButtonPressed(Button.B, function () {
-    bd(100)
+    pause(1000)
+    rt(90)
 })
-function reset () {
-    pins.servoWritePin(AnalogPin.P1, 90)
-    pins.servoWritePin(AnalogPin.P2, 90)
-}
 let timeToWait3 = 0
 let timeToWait4 = 0
 let timeToWait2 = 0
 let timeToWait = 0
-let DEGREESPERSEC = 0
+let DEGREESPERSECRT = 0
+let DEGREESPERSECLT = 0
 let DISTANCEPERSEC = 0
 let MICROSECINASEC = 0
 MICROSECINASEC = 1000000
 DISTANCEPERSEC = 100
-DEGREESPERSEC = 200
-reset()
+DEGREESPERSECRT = 250
+DEGREESPERSECLT = 270
+stop()
+basic.showIcon(IconNames.Happy)
+
